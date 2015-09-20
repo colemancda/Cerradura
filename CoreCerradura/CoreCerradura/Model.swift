d4e75378269d9e6e182edc83fb5d8ef18873a91f
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-20 15:12:49 -0500

    finished static model

--- CoreCerradura/CoreCerradura/Model.swift
+++ CoreCerradura/CoreCerradura/Model.swift
@@ -37,15 +37,15 @@ public extension Model {
            
             var entity = Entity()
             
-            entity.attributes[Attribute.Archived.name]      = Attribute.Archived.property
-            entity.attributes[Attribute.Created.name]       = Attribute.Created.property
-            entity.attributes[Attribute.Name.name]          = Attribute.Name.property
-            entity.attributes[Attribute.Secret.name]        = Attribute.Secret.property
-            entity.attributes[Attribute.Model.name]         = Attribute.Model.property
-            entity.attributes[Attribute.Version.name]       = Attribute.Version.property
-            entity.attributes[Attribute.FirmwareBuild.name] = Attribute.FirmwareBuild.property
+            entity.attributes[Attribute.Archived.name]          = Attribute.Archived.property
+            entity.attributes[Attribute.Created.name]           = Attribute.Created.property
+            entity.attributes[Attribute.Name.name]              = Attribute.Name.property
+            entity.attributes[Attribute.Secret.name]            = Attribute.Secret.property
+            entity.attributes[Attribute.Model.name]             = Attribute.Model.property
+            entity.attributes[Attribute.Version.name]           = Attribute.Version.property
+            entity.attributes[Attribute.FirmwareBuild.name]     = Attribute.FirmwareBuild.property
             
-            entity.relationships[Relationship.Actions.name] = Relationship.Actions.property
+            entity.relationships[Relationship.Actions.name]     = Relationship.Actions.property
             entity.relationships[Relationship.Permissions.name] = Relationship.Permissions.property
             
             return entity
@@ -109,10 +109,10 @@ public extension Model {
         public struct Function {
             
             /// Archives the entity.
-            public static let Archive           = "archive"
+            public static let Archive = "archive"
             
             /// Unlocks the lock.
-            public static let Unlock            = "unlock"
+            public static let Unlock = "unlock"
         }
     }
 }
@@ -124,36 +124,69 @@ public extension Model {
         
         public static let entityName = "User"
         
+        public static let entity: Entity = {
+            
+            var entity = Entity()
+            
+            entity.attributes[Attribute.Archived.name]          = Attribute.Archived.property
+            entity.attributes[Attribute.Created.name]           = Attribute.Created.property
+            entity.attributes[Attribute.Email.name]             = Attribute.Email.property
+            entity.attributes[Attribute.EmailValidated.name]    = Attribute.EmailValidated.property
+            entity.attributes[Attribute.Password.name]          = Attribute.Password.property
+            entity.attributes[Attribute.Username.name]          = Attribute.Username.property
+            
+            entity.relationships[Relationship.Actions.name]     = Relationship.Actions.property
+            entity.relationships[Relationship.Permissions.name] = Relationship.Permissions.property
+            
+            return entity
+            }()
+        
         public struct Attribute {
             
             /// Whether this entity is archived or not.
             ///
             /// Archived entities are basically deleted, but still stored for historical purposes.
-            public static let Archived          = "archived"
+            public static let Archived = (name: "archived", property: CoreModel.Attribute(type: .Number(.Boolean)))
             
             /// Date the user was created.
-            public static let Created           = "created"
+            public static let Created = (name: "created", property: CoreModel.Attribute(type: .Date))
             
             /// User's email address.
-            public static let Email             = "email"
+            public static let Email = (name: "email", property: CoreModel.Attribute(type: .String))
             
             /// Whether the email was validated.
-            public static let EmailValidated    = "emailValidated"
+            public static let EmailValidated = (name: "emailValidated", property: CoreModel.Attribute(type: .Number(.Boolean)))
             
             /// User's password (Used for authentication).
-            public static let Password          = "password"
+            public static let Password = (name: "password", property: CoreModel.Attribute(type: .String))
             
             /// User's username (Used for authentication).
-            public static let Username          = "username"
+            public static let Username = (name: "username", property: CoreModel.Attribute(type: .String))
         }
         
         public struct Relationship {
             
             /// Actions involving this user.
-            public static let Actions           = "actions"
+            public static let Actions: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                    optional: true,
+                    destinationEntityName: Action.entityName,
+                    inverseRelationshipName: Action.Relationship.User.name)
+                
+                return (name: "actions", property: property)
+                }()
             
             /// Permissions granted for this user.
-            public static let Permissions       = "permissions"
+            public static let Permissions: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                    optional: true,
+                    destinationEntityName: Permission.entityName,
+                    inverseRelationshipName: Permission.Relationship.User.name)
+                
+                return (name: "permissions", property: property)
+                }()
         }
         
         public struct Function {
@@ -171,18 +204,33 @@ public extension Model {
         
         public static let entityName = "Action"
         
+        public static let entity: Entity = {
+            
+            var entity = Entity()
+            
+            entity.attributes[Attribute.Date.name]              = Attribute.Date.property
+            entity.attributes[Attribute.ActionType.name]        = Attribute.ActionType.property
+            entity.attributes[Attribute.Status.name]            = Attribute.Status.property
+            
+            entity.relationships[Relationship.Lock.name]        = Relationship.Lock.property
+            entity.relationships[Relationship.User.name]        = Relationship.User.property
+            entity.relationships[Relationship.Permission.name]  = Relationship.Permission.property
+            
+            return entity
+            }()
+        
         public struct Attribute {
             
             /// Date this action ocurred.
-            public static let Date              = "date"
+            public static let Date = (name: "date", property: CoreModel.Attribute(type: .Date))
             
             /// Type of action. Raw value of ```ActionType```.
-            public static let Type              =  "type"
+            public static let ActionType = (name: "type", property: CoreModel.Attribute(type: .String))
             
             /// Whether the action has been completed. 
             ///
             /// Raw value of ```ActionStatus```.
-            public static let Status            = "status"
+            public static let Status = (name: "status", property: CoreModel.Attribute(type: .String))
         }
         
         public struct Relationship {
@@ -190,7 +238,7 @@ public extension Model {
             /// The lock associated with this action.
             public static let Lock: (name: String, property: CoreModel.Relationship) = {
                 
-                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
                     optional: true,
                     destinationEntityName: Model.Lock.entityName,
                     inverseRelationshipName: Model.Lock.Relationship.Actions.name)
@@ -199,10 +247,26 @@ public extension Model {
                 }()
             
             /// The user associated with this action.
-            public static let User              = "user"
+            public static let User: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
+                    optional: true,
+                    destinationEntityName: Model.User.entityName,
+                    inverseRelationshipName: Model.User.Relationship.Actions.name)
+                
+                return (name: "user", property: property)
+                }()
             
             /// The permission associated with this action.
-            public static let Permission        = "permission"
+            public static let Permission: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
+                    optional: true,
+                    destinationEntityName: Model.Permission.entityName,
+                    inverseRelationshipName: Model.Permission.Relationship.Actions.name)
+                
+                return (name: "permission", property: property)
+                }()
         }
     }
 }
@@ -215,36 +279,58 @@ public extension Model {
         
         public static let entityName = "Permission"
         
+        public static let entity: Entity = {
+            
+            var entity = Entity()
+            
+            entity.attributes[Attribute.Archived.name]              = Attribute.Archived.property
+            entity.attributes[Attribute.Created.name]               = Attribute.Created.property
+            entity.attributes[Attribute.StartDate.name]             = Attribute.StartDate.property
+            entity.attributes[Attribute.EndDate.name]               = Attribute.EndDate.property
+            entity.attributes[Attribute.ScheduledStartTime.name]    = Attribute.ScheduledStartTime.property
+            entity.attributes[Attribute.ScheduledEndTime.name]      = Attribute.ScheduledEndTime.property
+            entity.attributes[Attribute.PermissionType.name]        = Attribute.PermissionType.property
+            
+            entity.relationships[Relationship.Lock.name]            = Relationship.Lock.property
+            entity.relationships[Relationship.User.name]            = Relationship.User.property
+            entity.relationships[Relationship.Actions.name]         = Relationship.Actions.property
+            entity.relationships[Relationship.DerivedPermissions.name] = Relationship.DerivedPermissions.property
+            entity.relationships[Relationship.ParentPermission.name] = Relationship.ParentPermission.property
+            
+            return entity
+            }()
+        
         public struct Attribute {
             
             /// Whether this entity is archived or not.
             ///
             /// Archived entities are basically deleted, but still stored for historical purposes.
-            public static let Archived          = "archived"
+            public static let Archived = (name: "archived", property: CoreModel.Attribute(type: .Number(.Boolean)))
             
             /// Date the permission was created.
-            public static let Created           = "created"
+            public static let Created = (name: "created", property: CoreModel.Attribute(type: .Date))
             
             /// The date this permission goes into effect.
-            public static let StartDate         = "startDate"
+            public static let StartDate = (name: "startDate", property: CoreModel.Attribute(type: .Date))
+            
             
             /// The date this permission becomes invalid.
-            public static let EndDate           = "endDate"
+            public static let EndDate = (name: "endDate", property: CoreModel.Attribute(type: .Date, optional: true))
             
             /// The starting time of the time interval the lock can be unlocked.
             ///
             /// -Note: Not applicable for admin / anytime permissions.
-            public static let ScheduledStartTime = "scheduledStartTime"
+            public static let ScheduledStartTime = (name: "scheduledStartTime", property: CoreModel.Attribute(type: .Number(.Integer), optional: true))
             
             /// The ending time of the time interval the lock can be unlocked. 
             ///
             /// -Note: Not applicable for admin / anytime permissions.
-            public static let ScheduledEndTime  = "scheduledEndTime"
+            public static let ScheduledEndTime = (name: "scheduledEndTime", property: CoreModel.Attribute(type: .Number(.Integer), optional: true))
             
             /// Type of permission. 
             ///
             /// Raw value for ```PermissionType```.
-            public static let PermissionType    = "permissionType"
+            public static let PermissionType = (name: "permissionType", property: CoreModel.Attribute(type: .String))
         }
         
         public struct Relationship {
@@ -252,7 +338,7 @@ public extension Model {
             /// The lock this permission is granting access for.
             public static let Lock: (name: String, property: CoreModel.Relationship) = {
                 
-                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
                     optional: false,
                     destinationEntityName: Model.Lock.entityName,
                     inverseRelationshipName: Model.Lock.Relationship.Permissions.name)
@@ -261,16 +347,48 @@ public extension Model {
                 }()
             
             /// The user this permssion is granting access to. 
-            public static let User              = "user"
+            public static let User: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
+                    optional: false,
+                    destinationEntityName: Model.User.entityName,
+                    inverseRelationshipName: Model.User.Relationship.Permissions.name)
+                
+                return (name: "user", property: property)
+                }()
             
             /// Permissions derived from this permission. 
-            public static let DerivedPermissions = "derivedPermissions"
+            public static let DerivedPermissions: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                    optional: true,
+                    destinationEntityName: Model.Permission.entityName,
+                    inverseRelationshipName: Model.Permission.Relationship.ParentPermission.name)
+                
+                return (name: "derivedPermissions", property: property)
+                }()
             
             /// Permission this permission was derived from. 
-            public static let ParentPermission  = "parentPermission"
+            public static let ParentPermission: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToOne,
+                    optional: true,
+                    destinationEntityName: Model.Permission.entityName,
+                    inverseRelationshipName: Model.Permission.Relationship.DerivedPermissions.name)
+                
+                return (name: "parentPermission", property: property)
+                }()
             
             /// Actions involving this permission. 
-            public static let Actions           = "actions"
+            public static let Actions: (name: String, property: CoreModel.Relationship) = {
+                
+                let property = CoreModel.Relationship(type: RelationshipType.ToMany,
+                    optional: true,
+                    destinationEntityName: Action.entityName,
+                    inverseRelationshipName: Action.Relationship.Permission.name)
+                
+                return (name: "actions", property: property)
+                }()
         }
     }
 }
